205f06189de5c462789090f4d12563a8c32be17e
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-03-14 14:39:18 +0000

    refactor login form component state and submitHandler

--- src/App.js
+++ src/App.js
@@ -14,30 +14,14 @@ const App = () => {
   // }
 
   const [user, setUser] = useState();
-  const [username, setUsername] = useState();
-  const [email, setEmail] = useState();
-  const [pass, setPass] = useState();
-  const [bool, setBool] = useState(false);
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    setUser({username: username, email: email, pass: pass});
-  }
 
   return (
     <div className="App">
       <h1>React Full-Stack</h1>
       {/* if user exists render */}
-      {user && <h2>{user.username}</h2>}
+      {/* {user && <h2>{user.username}</h2>} */}
       {user ? <h1>{user.username}</h1> : <h1>Please type something...</h1>}
-     <Login 
-        handler={submitHandler} 
-        nameSetter={setUsername} 
-        emailSetter={setEmail} 
-        passSetter={setPass} 
-        boolSetter={setBool}
-        bool={bool}
-      />
+     <Login setUser={user} />
     </div>
   );
 };
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
 
 
-const Login = ({ handler, nameSetter, emailSetter, passSetter, boolSetter, bool }) => {
+const Login = ({ setUser }) => {
+
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [pass, setPass] = useState();
+  const [bool, setBool] = useState(false);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setUser({username: username, email: email, pass: pass});
+  }
 
   return (
     <>
-      <form onSubmit={handler}>
-        <input onChange={(event) => nameSetter(event.target.value)} placeholder="Username" />
+      <form onSubmit={submitHandler}>
+        <input onChange={(event) => setUsername(event.target.value)} placeholder="Username" />
         {/* only render email input if bool is false */}
-        {!bool && <input onChange={(event) => emailSetter(event.target.value)} placeholder="Email" />}
-        <input onChange={(event) => passSetter(event.target.value)} placeholder="Password" />
+        {!bool && <input onChange={(event) => setEmail(event.target.value)} type="email" placeholder="Email" />}
+        <input onChange={(event) => setPass(event.target.value)} type="password" placeholder="Password" />
         <button type='submit'>Submit</button>
       </form>
       {/* Set bool to opposite */}
-      <button onClick={() => boolSetter(!bool)}>Login or sign-up</button>
+      <button onClick={() => setBool(!bool)}>Login or sign-up</button>
     </>
   );
 }
