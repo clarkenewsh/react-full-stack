57e1f269aa56de9149ad3c749a57e18e569f38a7
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-03-15 15:11:16 +0000

    photContainer component created to output lorem pic api response

---
+++ src/components/PhotoContainer/PhotoContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const PhotoContainer = ({ photo }) => {
+  return (
+    <>
+      <p>{photo.author}</p>
+      <img src={photo.download_url} alt="lorem picsum random" />
+    </>
+  )
+}
+
+export default PhotoContainer
