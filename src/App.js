09accb72c080bcc20f827355f1d13137e0848ce9
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-03-15 15:18:51 +0000

    App component refactored to show login if no user state or show home if user state exists, environemnt variables created for react app rest api url and added to gitignore

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState } from 'react';
 import Login from './components/login/login';
+import Home from './components/home/Home';
 
 const App = () => {
 
@@ -19,9 +20,10 @@ const App = () => {
     <div className="App">
       <h1>React Full-Stack</h1>
       {/* if user exists render */}
-      {/* {user && <h2>{user.username}</h2>} */}
-      {user ? <h1>{user.username}</h1> : <h1>Please type something...</h1>}
-     <Login setUser={user} />
+      {user && <h2>{user}</h2>}
+      {user ? <h1>{user}</h1> : <h1>Please type something...</h1>}
+      {/* If not logged in render login. If logged in render home page*/}
+     {!user ? <Login setUser={setUser} /> : <Home />}
     </div>
   );
 };
