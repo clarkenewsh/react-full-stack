88d42646cfc3b21a48efa870577fe2d9fbe5f8cc
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-03-16 18:37:07 +0000

    PhotoContainer component refactor html and styling added

--- src/components/PhotoContainer/PhotoContainer.js
+++ src/components/PhotoContainer/PhotoContainer.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './PhotoContainer.css';
 
 
 const PhotoContainer = ({ photo }) => {
   return (
-    <>
+    <figure className='photo-card'>
+      <img src={photo.download_url} alt="lorem picsum random pic" />
       <p>{photo.author}</p>
-      <img src={photo.download_url} alt="lorem picsum random" />
-    </>
+    </figure>
   )
 }
 
