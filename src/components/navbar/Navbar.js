0d8cc92e68012b7f0b38abf49557ffd1ce3bd45f
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-03-16 18:36:14 +0000

    Navbar component created with conditional rendering logout button, styling added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import './Navbar.css';
 
-const Navbar = () => {
+const Navbar = ({ user, logoutHandler }) => {
   return (
     <>
-      <nav>
+      <nav className='wrapper'>
+        <a id="logo">Instagram</a>
         {/* <Link to="/">Login / Register</Link> */}
+        {user && <p>Logged In: {user}</p>}
+        {user && <button onClick={logoutHandler}>Logout</button>}
       </nav>
     </>
   )
