c7d4c8fed75f3451777959bd8149d7e7729e1d3d
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-03-16 18:37:53 +0000

    App comonent refactor

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import Login from './components/login/Login';
 import Home from './components/home/Home';
 import { tokenLogin } from './utils';
+import Navbar from './components/navbar/Navbar';
 import './App.css';
 
 const App = () => {
@@ -25,15 +26,24 @@ const App = () => {
     }
   }, []);
 
+  const logoutHandler = () => {
+    localStorage.removeItem("myToken");
+    setUser(null);
+  }
+
   return (
-    <div>
-      <h1>React Full-Stack</h1>
-      {/* if user exists render */}
-      {user && <h2>{user}</h2>}
-      {user ? <h1>{user}</h1> : <h1>Please type something...</h1>}
-      {/* If not logged in render login. If logged in render home page */}
-      {!user ? <Login setUser={setUser} /> : <Home />} 
-     </div>
+    <>
+      <header>
+        <Navbar user={user} logoutHandler={logoutHandler} />
+      </header>
+      <main className='wrapper'>
+        {/* if user exists render */}
+        {/* {user && <h2>{user}</h2>} */}
+        {user ? <h1>Welcome, {user}</h1> : <h1>Login / Register</h1>}
+        {/* If not logged in render login. If logged in render home page */}
+        {!user ? <Login setUser={setUser} /> : <Home />} 
+      </main>
+     </>
     //  <BrowserRouter className="App">
     //  <Routes>
     //    <Route path='/' element={ <Login setUser={setUser} user={user} /> } />
